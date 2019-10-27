8d2ecac853ee65de196db650bad9616b971967b1
mongoose <36258286+MY201314MY@users.noreply.github.com>
2019-10-27 17:31:37 +0800

    String array

---
+++ C++/C/string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	void Init_screen();
+
+	char str[3][20]={"hello","Dood morning!","Thank you."};
+	unsigned char i;
+
+	Init_screen();
+
+	for(i=0;i<3;i++)
+	{
+		printf("%s",str[i]);
+		printf("\n");
+	}
+
+	puts("\n");
+
+	return 0;
+}
+
+void Init_screen()
+{
+	printf("****************************\n");
+    printf("Welcome to enter the system!\n");
+	printf("****************************\n");
+
+	puts("\n");
+}
