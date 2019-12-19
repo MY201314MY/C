80b5a75ecfb0957e7f451aadff402ea760145df9
mongoose <36258286+MY201314MY@users.noreply.github.com>
2019-12-20 00:15:12 +0800

    Update struct-simple.c

--- C/struct-simple.c
+++ C/struct-simple.c
@@ -3,26 +3,26 @@
 char*formal="%s\t%s\t%c\n";
 typedef struct
 {
-	char number[10];
-	char name [20];
-	char sex;
+char number[10];
+char name [20];
+char sex;
 }student;
 
 int main()
 {
-	student s,*p;
-  int a=9,*p1;
-	p1=&a;
-	printf("%X\t",p1);
-	printf("%d\n",sizeof(a));
+student s,*p;
+int a=9,*p1;
+p1=&a;
+printf("%X\t",p1);
+printf("%d\n",sizeof(a));
 
-	strcpy(s.number,"123456");
-	strcpy(s.name,"Li ming");
-  s.sex='F';
-  p=&s;
-	printf("%X\n",p);
-	printf("%s\t%s\t%c\n",s.number,s.name,s.sex);
-  printf(formal,p->number,p->name,p->sex);
+strcpy(s.number,"123456");
+strcpy(s.name,"Li ming");
+s.sex='F';
+p=&s;
+printf("%X\n",p);
+printf("%s\t%s\t%c\n",s.number,s.name,s.sex);
+printf(formal,p->number,p->name,p->sex);
 
-	return 0;
+return 0;
 }
